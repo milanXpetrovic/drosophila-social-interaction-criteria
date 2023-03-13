ce3277bf34a29f1b2f227a7654785e91bddd1721
milanXpetrovic <dr.milojko@gmail.com>
2023-03-13 23:45:53 +0100

    fix: better histogram and ploting

--- fly_pipe/find_edges/automated_schneider/0_0_angle_dist_in_group.py
+++ fly_pipe/find_edges/automated_schneider/0_0_angle_dist_in_group.py
@@ -20,6 +20,10 @@ normalization = json.load(open("../../../data/normalization.json"))
 pxpermm = json.load(open("../../../data/pxpermm/" + POP + ".json"))
 
 treatment = fileio.load_multiple_folders(PATH)
+
+degree_bins = np.arange(0, 361, 5)
+distance_bins = np.arange(0, 6.251, 0.25)
+
 for group_name, group_path in treatment.items():
     print(group_name)
     group = fileio.load_files_from_folder(group_path, file_format='.csv')
@@ -31,30 +35,22 @@ for group_name, group_path in treatment.items():
 
     norm = normalization[group_name]
 
-    # for fly in group.keys():
-    #     df1 = group_dfs[fly]
-
     for fly1, fly2 in combinations:
         df1 = group_dfs[fly1].copy(deep=True)
         df2 = group_dfs[fly2].copy(deep=True)
         df = pd.DataFrame()
 
-        df['movement'] = ((df1['pos x'] - df1['pos x'].shift())
-                          ** 2 + (df1['pos y'] - df1['pos y'].shift())**2)**0.5
+        df['movement_df1'] = ((df1['pos x'] - df1['pos x'].shift())
+                              ** 2 + (df1['pos y'] - df1['pos y'].shift())**2)**0.5
 
-        df['movement'] = df['movement']/pxpermm[group_name]*FPS
-        df.loc[0, 'movement'] = df.loc[1, 'movement']
+        df.loc[0, 'movement_df1'] = df.loc[1, 'movement_df1']
 
-        n, c = np.histogram(df['movement'].values,
+        df['movement_df1'] = df['movement_df1']/pxpermm[group_name]/FPS
+        n, c = np.histogram(df['movement_df1'].values,
                             bins=np.arange(0, 2.51, 0.01))
         opp = np.max(n) - n
         peaks, properties = find_peaks(opp/np.max(opp), prominence=0.05)
-
-        if len(peaks) == 0:
-            movecut = 0  # 0.01
-
-        else:
-            movecut = c[peaks[0]]
+        movecut_df1 = 0 if len(peaks) == 0 else c[peaks[0]]
 
         df2["pos x"] = (df2["pos x"] - norm["x"])  # / norm["radius"]
         df2["pos y"] = (df2["pos y"] - norm["y"])  # / norm["radius"]
@@ -84,21 +80,33 @@ for group_name, group_path in treatment.items():
         df['qx2'] = df1['pos x'] + cos * df["dfx"] - sin * df["dfy"]
         df['qy2'] = df1['pos y'] + sin * df["dfx"] + cos * df["dfy"]
 
-        df['angle'] = np.arctan2(df["qy2"], df["qx2"])
+        df['angle'] = np.arctan2(df["dfy"], df["dfx"])
         df['angle'] = np.rad2deg(df['angle'])
         df['angle'] = np.round(df['angle'])
 
+        df["angle_diff"] = (np.rad2deg(df1["ori"])) - df["angle"]
+
+        while df["angle_diff"].min() < -180 and df["angle_diff"].max() > 180:
+            df.loc[df['angle_diff'] < -180, 'angle_diff'] += 360
+            df.loc[df['angle_diff'] > 180, 'angle_diff'] -= 360
+
+        df["angle"] = np.round(df["angle_diff"])
+
         df = df[df.distance <= 7]
-        df = df[df.movement > movecut]
+        # df = df[df.movement_df1 > (movecut_df1*pxpermm[group_name]/FPS)]
 
         df = df.groupby(['angle', 'distance']
                         ).size().reset_index(name='counts')
 
         total = pd.concat([total, df], axis=0)
 
+    # histogram and bins
+    # hist, _, _ = np.histogram2d(df['angle'], df['distance'], bins=(
+    #     degree_bins, distance_bins), weights=df['counts'])
+
     total.to_csv("{}/{}.csv".format(OUTPUT_PATH, group_name))
 
-
+# %%
 group = fileio.load_files_from_folder(OUTPUT_PATH, file_format='.csv')
 
 degree_bins = np.arange(0, 361, 5)
@@ -115,11 +123,14 @@ for name, path in group.items():
     norm_hist = np.ceil((hist / np.max(hist)) * 256)
     res += norm_hist
 
-fig, ax = plt.subplots(figsize=(10, 10), subplot_kw={'polar': True})
-img = ax.pcolormesh(np.radians(degree_bins), distance_bins, res.T, cmap='jet')
+res = np.ceil((res / np.max(res)) * 256)
 
-ax.set_rgrids(np.arange(0, 6.251, 0.5), angle=0)
+fig, ax = plt.subplots(figsize=(8, 8), subplot_kw={'polar': True})
+
+img = ax.pcolormesh(np.radians(degree_bins), distance_bins, res.T, cmap="jet")
+
+ax.set_rgrids(np.arange(0, 6.251, 1.0), angle=0)
 ax.grid(True)
-
+plt.title("MOVECUT * pxpermm[group_name] / FPS")
 plt.tight_layout()
 plt.show()
