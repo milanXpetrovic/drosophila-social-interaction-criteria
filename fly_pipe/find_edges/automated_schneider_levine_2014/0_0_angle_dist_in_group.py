1fd75d238339d7661e3353daae9128e794440cc5
milanXpetrovic <dr.milojko@gmail.com>
2023-03-19 16:41:24 +0100

    add: numpy foo for angle and distance

--- fly_pipe/find_edges/automated_schneider_levine_2014/0_0_angle_dist_in_group.py
+++ fly_pipe/find_edges/automated_schneider_levine_2014/0_0_angle_dist_in_group.py
@@ -1,5 +1,5 @@
 # %%
-from typing import Dict
+from typing import Dict, Tuple
 import random
 import os
 import sys
@@ -16,19 +16,7 @@ from fly_pipe.utils import fileio
 
 
 def pick_random_group(treatment: Dict[str, str], group_size: int) -> Dict[str, str]:
-    """
-    Randomly picks a group of flies from each treatment.
-
-    Args:
-        treatment: A dictionary containing treatment names as keys and
-                   paths to folders containing fly data as values.
-        group_size: An integer representing the number of flies to pick
-                    from each treatment.
-
-    Returns:
-        A dictionary where keys are in the format "{group_name}-{fly_name}"
-        and values are paths to the selected fly data.
-    """
+    """Randomly picks a group of flies from each treatment."""
 
     n = len(treatment)-1
     m = group_size
@@ -52,18 +40,8 @@ def pick_random_group(treatment: Dict[str, str], group_size: int) -> Dict[str, s
     return random_pick
 
 
-def angledifference_nd(angle1: pd.Series, angle2: pd.Series) -> pd.Series:
-    """
-    Calculates the difference between two angles in degrees.
-
-    Args:
-        angle1: A pandas Series representing the first angle in degrees.
-        angle2: A pandas Series representing the second angle in degrees.
-
-    Returns:
-        A pandas Series representing the difference between the two angles
-        in degrees, adjusted to be in the range [-180, 180].
-    """
+def angledifference_nd(angle1, angle2):
+    """Calculates the difference between two angles in degrees."""
 
     difference = angle2 - angle1
     adjustlow = difference < -180
@@ -75,6 +53,35 @@ def angledifference_nd(angle1: pd.Series, angle2: pd.Series) -> pd.Series:
         adjusthigh = difference > 180
 
     return difference
+
+
+def normalize_random_group(random_group: Dict[str, str],
+                           normalization: Dict[str, Dict[str, float]],
+                           pxpermm: Dict[str, float]) -> Tuple[Dict[str, pd.DataFrame], Dict[str, float]]:
+    """
+    Normalize the data in each CSV file in `random_group` using the normalization values in `normalization` 
+    and `pxpermm`.
+    """
+
+    normalized_dfs = {}
+    pxpermm_dict = {}
+    for group, fly_path in random_group.items():
+        norm = normalization[group]
+        pxpermm_group = pxpermm[group] / (2 * norm["radius"])
+
+        df = pd.read_csv(fly_path, index_col=0)
+
+        df["pos x"] = (df["pos x"] - norm["x"] +
+                       norm["radius"]) / (2 * norm["radius"])
+        df["pos y"] = (df["pos y"] - norm["y"] +
+                       norm["radius"]) / (2 * norm["radius"])
+
+        df["a"] = df["a"] / (2*norm["radius"])
+
+        normalized_dfs.update({group: df})
+        pxpermm_dict.update({group: pxpermm_group})
+
+    return (normalized_dfs, pxpermm)
 
 
 def group_space_angle_hist(group: Dict[str, str], norm: Dict[str, float], pxpermm_group: float) -> np.ndarray:
@@ -149,7 +156,16 @@ def group_space_angle_hist(group: Dict[str, str], norm: Dict[str, float], pxperm
     return norm_total
 
 
-def random_group_space_angle_hist(normalized_dfs, pxpermm):
+def random_group_space_angle_hist(normalized_dfs: dict[str, pd.DataFrame], pxpermm: dict[str, float]) -> np.ndarray:
+    """Calculate and return a 2D histogram of the angular and distance differences between pairs of flies based on their positions, using normalized dataframes.
+
+    Args:
+    normalized_dfs (Dict[str, pd.DataFrame]): A dictionary containing the normalized dataframes of the flies.
+    pxpermm (Dict[str, float]): A dictionary containing the conversion factor from pixels to millimeters for each fly.
+
+    Returns:
+    A numpy array representing the normalized 2D histogram of the angular and distance differences between pairs of flies.
+    """
 
     degree_bins = np.arange(-177.5, 177.6, 5)
     distance_bins = np.arange(0.125, 99.8751, 0.25)
@@ -193,29 +209,6 @@ def random_group_space_angle_hist(normalized_dfs, pxpermm):
     norm_total = np.ceil((total / np.max(total)) * 256)
 
     return norm_total
-
-
-def normalize_random_group(random_group, normalization, pxpermm):
-
-    normalized_dfs = {}
-    pxpermm_dict = {}
-    for group, fly_path in random_group.items():
-        norm = normalization[group]
-        pxpermm_group = pxpermm[group] / (2 * norm["radius"])
-
-        df = pd.read_csv(fly_path, index_col=0)
-
-        df["pos x"] = (df["pos x"] - norm["x"] +
-                       norm["radius"]) / (2 * norm["radius"])
-        df["pos y"] = (df["pos y"] - norm["y"] +
-                       norm["radius"]) / (2 * norm["radius"])
-
-        df["a"] = df["a"] / (2*norm["radius"])
-
-        normalized_dfs.update({group: df})
-        pxpermm_dict.update({group: pxpermm_group})
-
-    return (normalized_dfs, pxpermm)
 
 
 def plot_heatmap(histogram):
@@ -265,13 +258,86 @@ res = np.sum([np.load(path) for path in group.values()], axis=0)
 
 # %%
 
+
+def test_np_hist(normalized_dfs: dict[str, pd.DataFrame], pxpermm: dict[str, float]) -> np.ndarray:
+
+    degree_bins = np.arange(-177.5, 177.6, 5)
+    distance_bins = np.arange(0.125, 99.8751, 0.25)
+    total = np.zeros((len(degree_bins)-1, len(distance_bins)-1))
+
+    for fly1_key, fly2_key in list(itertools.permutations(normalized_dfs.keys(), 2)):
+        df1 = normalized_dfs[fly1_key].copy(deep=True)
+        df2 = normalized_dfs[fly2_key].copy(deep=True)
+
+        df1_array = df1.to_numpy()
+        df2_array = df2.to_numpy()
+
+        movement = np.sqrt((df1_array[:, 0] - np.roll(df1_array[:, 0], 1))**2
+                           + (df1_array[:, 1] - np.roll(df1_array[:, 1], 1))**2)
+        movement[0] = movement[1]
+        movement = movement / pxpermm[fly1_key] / FPS
+
+        n, c = np.histogram(movement, bins=np.arange(0, 2.51, 0.01))
+
+        peaks, _ = find_peaks(
+            (np.max(n) - n) / np.max(np.max(n) - n), prominence=0.05)
+        movecut = 0 if len(peaks) == 0 else c[peaks[0]]
+
+        a = np.mean(df1_array[:, 3])
+        distance = np.sqrt((df1_array[:, 0] - df2_array[:, 0])**2
+                           + (df1_array[:, 1] - df2_array[:, 1])**2)
+        distance = np.round(distance / (a * 4), 4)
+
+        checkang = np.arctan2(
+            df2_array[:, 1] - df1_array[:, 1], df2_array[:, 0] - df1_array[:, 0])
+        checkang = checkang * 180 / np.pi
+
+        angle = angledifference_nd(checkang, df1_array[:, 2])
+        angle = np.round(angle)
+
+        mask = (distance <= 100) & (movement > (
+            movecut * pxpermm[fly1_key] / FPS))
+
+        angle = angle[mask]
+        distance = distance[mask]
+
+        hist, _, _ = np.histogram2d(angle, distance, bins=(
+            degree_bins, distance_bins), range=[[-180, 180], [0, 100.0]])
+
+        total += hist
+
+    norm_total = np.ceil((total / np.max(total)) * 256)
+
+    return norm_total
+
+
 all_hists = []
-for i in range(100):
+for i in range(1):
+    t1 = time.time()
     random_group = pick_random_group(treatment, group_size=12)
+    t2 = time.time()
+    print(f"Time taken for pick_random_group: {t2-t1:.2f} seconds")
+
+    t1 = time.time()
     normalized_dfs, pxpermm = normalize_random_group(
         random_group, normalization, pxpermm)
-    hist = random_group_space_angle_hist(normalized_dfs, pxpermm)
+    t2 = time.time()
+    print(f"Time taken for normalize_random_group: {t2-t1:.2f} seconds")
+
+    t1 = time.time()
+    hist_df = random_group_space_angle_hist(normalized_dfs, pxpermm)
+    t2 = time.time()
+    print(f"Time taken for random_group_space_angle_hist: {t2-t1:.2f} seconds")
+
+    t1 = time.time()
+    hist_np = test_np_hist(normalized_dfs, pxpermm)
+    t2 = time.time()
+    print(f"Time taken for test_np_hist: {t2-t1:.2f} seconds")
+
     all_hists.append(hist)
+
+# %%
+
 
 # %%
 res = np.sum(all_hists, axis=0)
