0d60d31628ba9095452e37ce0e964751af5f8f4d
milanXpetrovic <dr.milojko@gmail.com>
2023-03-30 18:27:02 +0200

    fix: paths updated

--- fly_pipe/settings.py
+++ fly_pipe/settings.py
@@ -1,33 +1,29 @@
 import os
-# root = os.path.dirname(os.path.dirname(__file__))
+
 TREATMENT = "CSf"
-# TREATMENT_NAME =
-
-
-RAW_DATA = r"F:\fly-pipe\data\raw"
-
-
-# TREATMENT_PATH
-
-# os.path.join()
-PXPERMM = "../../../data/pxpermm/" + TREATMENT + ".json"
-
-
-NROMALIZATION = r"F:\fly-pipe\data\normalization.json"
-
 
 FPS = 22.8
-
-RANDOM_GROUP_SIZE = 12
+START = 0
+END = 30
 
 DEGREE_BIN_SIZE = 5
 DISTANCE_BIN_SIZE = 0.25
-
 DISTANCE_MAX = 100
 
-MOVECUT = 1
-
+RANDOM_GROUP_SIZE = 12
 N_RANDOM_PASSES = 500
 
-START = 0
-END = 30
+MOVECUT = True
+
+# Paths
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
+
+# input files
+INPUT_DIR = os.path.join(ROOT_DIR, 'data', 'input')
+TRACKINGS = os.path.join(INPUT_DIR, 'trackings', TREATMENT)
+PXPERMM = os.path.join(INPUT_DIR, 'pxpermm', f'{TREATMENT}.json')
+NROMALIZATION = os.path.join(INPUT_DIR, 'normalization.json')
+
+
+
