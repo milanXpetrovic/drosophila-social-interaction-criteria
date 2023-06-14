e950f280feb080e772febfaf3d62191def13dc3c
milky-boi <dr.milojko@gmail.com>
2023-06-14 11:38:34 +0000

    changed from module to script

--- fly_pipe/settings.py
+++ fly_pipe/settings.py
@@ -5,23 +5,26 @@ TREATMENT = "CSf"
 FPS = 22.8
 START = 0
 END = 30
+EXP_DURATION = 30
+TIMECUT = 0
 
-DEGREE_BIN_SIZE = 5
-DISTANCE_BIN_SIZE = 0.25
+ANGLE_BIN = 5
+DISTANCE_BIN = 0.25
 DISTANCE_MAX = 100
 
 RANDOM_GROUP_SIZE = 15
-N_RANDOM_PASSES = 500
+N_RANDOM_1 = 500
+N_RANDOM_2 = 500
 
 MOVECUT = True
 
 # Paths
 # ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
-ROOT_DIR = "/home/mile/fly-pipe"
-LOGS_DIR = os.path.join(ROOT_DIR, 'logs')
+ROOT_DIR = "/home/milky/fly-pipe"
+LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 
 # Input files
-INPUT_DIR = os.path.join(ROOT_DIR, 'data', 'input')
-TRACKINGS = os.path.join(INPUT_DIR, 'trackings', TREATMENT)
-PXPERMM = os.path.join(INPUT_DIR, 'pxpermm', f'{TREATMENT}.json')
-NROMALIZATION = os.path.join(INPUT_DIR, 'normalization.json')
+INPUT_DIR = os.path.join(ROOT_DIR, "data", "input")
+TRACKINGS = os.path.join(INPUT_DIR, "trackings", TREATMENT)
+PXPERMM = os.path.join(INPUT_DIR, "pxpermm", f"{TREATMENT}.json")
+NROMALIZATION = os.path.join(INPUT_DIR, "normalization.json")
