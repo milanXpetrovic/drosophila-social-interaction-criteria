bc13d98b16d7df20f4429aa4858a5f007f99655e
milanXpetrovic <milan.petrovic@uniri.hr>
2024-09-20 10:20:53 +0000

    fixed code style

--- __main__.py
+++ __main__.py
@@ -5,7 +5,6 @@ import logging
 import multiprocessing
 import os
 import random
-import sys
 import time
 
 import natsort
@@ -34,7 +33,8 @@ log_file_path = os.path.join(LOGS_DIR, "runtime_log.txt")
 logger = logging.getLogger("runtime_logger")
 logger.setLevel(logging.INFO)
 
-if logger.hasHandlers():logger.handlers.clear()
+if logger.hasHandlers():
+    logger.handlers.clear()
 
 file_handler = logging.FileHandler(log_file_path)
 file_handler.setLevel(logging.INFO)
@@ -66,204 +66,219 @@ else:
     logger.info(f'{treatment_name} - Start from 0')
 
 while len(criteria_df) < 500:
-    total_time = time.time()
-    temp_ind = random.sample(range(len(treatment)), settings.RANDOM_GROUP_SIZE)
-    pick_random_groups = {list(treatment.keys())[i]: list(treatment.values())[i] for i in temp_ind}
-    
-    treatment_items = treatment.items()
-    with multiprocessing.Pool() as pool: all_hists = pool.starmap(SL.process_norm_group, treatment_items)
+    try:
+        total_time = time.time()
+        temp_ind = random.sample(range(len(treatment)), settings.RANDOM_GROUP_SIZE)
+        pick_random_groups = {list(treatment.keys())[i]: list(treatment.values())[i] for i in temp_ind}
 
-    superN = np.sum(all_hists, axis=0)
-    pseudo_N = SL.boot_pseudo_fly_space(treatment, temp_ind)
-    N2 = (superN / np.sum(superN)) - (pseudo_N / np.sum(pseudo_N))
-    falloff = np.arange(1, N2.shape[0] + 1).astype(float) ** -1
-    N2 = N2 * np.tile(falloff, (N2.shape[1], 1)).T
-    N2[N2 <= np.percentile(N2[N2 > 0], 95)] = 0
+        treatment_items = treatment.items()
+        with multiprocessing.Pool() as pool:
+            all_hists = pool.starmap(SL.process_norm_group, treatment_items)
 
-    C = {}
-    C[0] = np.arange(0, settings.DISTANCE_MAX, settings.DISTANCE_BIN)
-    C[1] = np.arange(-180, 181, settings.ANGLE_BIN)
-    a, b = np.where(N2 > 0)
-    tempangle, tempdistance = np.max(np.abs(C[1][b - 1])), C[0][np.max(a - 1)]
+        superN = np.sum(all_hists, axis=0)
+        pseudo_N = SL.boot_pseudo_fly_space(treatment, temp_ind)
+        N2 = (superN / np.sum(superN)) - (pseudo_N / np.sum(pseudo_N))
+        falloff = np.arange(1, N2.shape[0] + 1).astype(float) ** -1
+        N2 = N2 * np.tile(falloff, (N2.shape[1], 1)).T
+        N2[N2 <= np.percentile(N2[N2 > 0], 95)] = 0
 
-    h = np.array([
-            [0.0181, 0.0492, 0.0492, 0.0181],[0.0492, 0.1336, 0.1336, 0.0492],
-            [0.0492, 0.1336, 0.1336, 0.0492],[0.0181, 0.0492, 0.0492, 0.0181],
-        ])
-    h /= np.sum(h)
+        C = {}
+        C[0] = np.arange(0, settings.DISTANCE_MAX, settings.DISTANCE_BIN)
+        C[1] = np.arange(-180, 181, settings.ANGLE_BIN)
+        a, b = np.where(N2 > 0)
+        tempangle, tempdistance = np.max(np.abs(C[1][b - 1])), C[0][np.max(a - 1)]
 
-    N2 = convolve2d(N2, h, mode="same")
-    N2_int = np.where(N2 > 0, 1, N2)
-    labeled_image, num_labels = skimage_label.label(N2_int, connectivity=2, return_num=True)
+        h = np.array([
+                [0.0181, 0.0492, 0.0492, 0.0181], [0.0492, 0.1336, 0.1336, 0.0492],
+                [0.0492, 0.1336, 0.1336, 0.0492], [0.0181, 0.0492, 0.0492, 0.0181],
+            ])
+        h /= np.sum(h)
 
-    CC = {"Connectivity": 8}
-    CC["ImageSize "] = labeled_image.shape
-    CC["NumObjects"] = num_labels
-    CC["PixelIdxList"] = [np.where(labeled_image == label_num) for label_num in range(1, num_labels + 1)]
+        N2 = convolve2d(N2, h, mode="same")
+        N2_int = np.where(N2 > 0, 1, N2)
+        labeled_image, num_labels = skimage_label.label(N2_int, connectivity=2, return_num=True)
 
-    bcenter = np.where(C[0] < 2)[0][-5:]
-    acenter1, acenter2 = np.where(C[1] == -angle_bin * 2)[0][0], np.where(C[1] == angle_bin * 2)[0][0]
-    test = np.zeros_like(N2)
-    test[bcenter[0] : bcenter[-1] + 1, acenter1 : acenter2 + 1] = 1
-    G = np.where(test != 0)
+        CC = {"Connectivity": 8}
+        CC["ImageSize "] = labeled_image.shape
+        CC["NumObjects"] = num_labels
+        CC["PixelIdxList"] = [np.where(labeled_image == label_num) for label_num in range(1, num_labels + 1)]
 
-    for i in range(CC["NumObjects"]):
-        CC_pixel_idx_list = CC["PixelIdxList"][i]
-        CC_set = set(zip(*CC_pixel_idx_list))
-        G_set = set(zip(*G))
+        bcenter = np.where(C[0] < 2)[0][-5:]
+        acenter1, acenter2 = np.where(C[1] == -angle_bin * 2)[0][0], np.where(C[1] == angle_bin * 2)[0][0]
+        test = np.zeros_like(N2)
+        test[bcenter[0]: bcenter[-1] + 1, acenter1: acenter2 + 1] = 1
+        G = np.where(test != 0)
 
-        if len(CC_set & G_set) == 0: N2[CC["PixelIdxList"][i]] = 0
+        for i in range(CC["NumObjects"]):
+            CC_pixel_idx_list = CC["PixelIdxList"][i]
+            CC_set = set(zip(*CC_pixel_idx_list))
+            G_set = set(zip(*G))
 
-    if not np.any(N2 > 0): continue
+            if len(CC_set & G_set) == 0:
+                N2[CC["PixelIdxList"][i]] = 0
 
-    N2[N2 < np.percentile(N2[N2 > 0], 75)] = 0
-    N2_int = np.where(N2 > 0, 1, N2)
-    labeled_image, num_labels = skimage_label.label(N2_int, connectivity=2, return_num=True)
+        if not np.any(N2 > 0):
+            continue
 
-    CC["ImageSize "] = labeled_image.shape
-    CC["NumObjects"] = num_labels
-    CC["PixelIdxList"] =  [np.where(labeled_image == label_num) for label_num in range(1, num_labels + 1)]
+        N2[N2 < np.percentile(N2[N2 > 0], 75)] = 0
+        N2_int = np.where(N2 > 0, 1, N2)
+        labeled_image, num_labels = skimage_label.label(N2_int, connectivity=2, return_num=True)
 
-    num_pixels = np.array([len(pixel_idx) for pixel_idx in CC["PixelIdxList"]])
-    idx = np.where(num_pixels < 5)[0]
-    N3 = np.copy(N2) 
+        CC["ImageSize "] = labeled_image.shape
+        CC["NumObjects"] = num_labels
+        CC["PixelIdxList"] = [np.where(labeled_image == label_num) for label_num in range(1, num_labels + 1)]
 
-    for i in range(CC["NumObjects"]):
-        CC_pixel_idx_list = CC["PixelIdxList"][i]
-        CC_set = set(zip(*CC_pixel_idx_list))
-        G_set = set(zip(*G))
-        intersection = CC_set & G_set
+        num_pixels = np.array([len(pixel_idx) for pixel_idx in CC["PixelIdxList"]])
+        idx = np.where(num_pixels < 5)[0]
+        N3 = np.copy(N2)
 
-        if len(intersection) == 0: N2[CC["PixelIdxList"][i]] = 0
+        for i in range(CC["NumObjects"]):
+            CC_pixel_idx_list = CC["PixelIdxList"][i]
+            CC_set = set(zip(*CC_pixel_idx_list))
+            G_set = set(zip(*G))
+            intersection = CC_set & G_set
 
-    a, b = np.where(N2 > 0)
+            if len(intersection) == 0:
+                N2[CC["PixelIdxList"][i]] = 0
 
-    if len(a) == 0:
-        N2 = np.copy(N3)
-        for i in range(len(idx)):
-            N2[CC["PixelIdxList"][idx[i]]] = 0
         a, b = np.where(N2 > 0)
 
-    if not len(a) or not len(b): continue
+        if len(a) == 0:
+            N2 = np.copy(N3)
+            for i in range(len(idx)):
+                N2[CC["PixelIdxList"][idx[i]]] = 0
+            a, b = np.where(N2 > 0)
 
-    tempangle, tempdistance = np.max(np.abs(C[1][b])), C[0][np.max(a)]
-    N2 = superN / n - pseudo_N / nrand2
-    meanN2 = np.mean(N2)
-    storeN, storeT = np.zeros((len(C[0]) - 1, len(C[1]))), np.zeros((len(np.arange(0, 30 * 60, 0.05)), nrand1))
-    
-    keepitgoing = True
-    if tempangle.size != 0 and tempdistance is not None:
-        storeN = storeN + (superN / np.sum(superN) - pseudo_N / np.sum(pseudo_N)) / nrand1
+        if not len(a) or not len(b):
+            continue
 
-        while keepitgoing:
-            temp = N2[
-                np.where(C[0] == 1)[0][0] : np.where(C[0] == tempdistance)[0][0] + 1,
-                np.where(C[1] == -tempangle)[0][0] : np.where(C[1] == tempangle)[0][0] + 1
-            ]
+        tempangle, tempdistance = np.max(np.abs(C[1][b])), C[0][np.max(a)]
+        N2 = superN / n - pseudo_N / nrand2
+        meanN2 = np.mean(N2)
+        storeN, storeT = np.zeros((len(C[0]) - 1, len(C[1]))), np.zeros((len(np.arange(0, 30 * 60, 0.05)), nrand1))
 
-            tempmean = temp.mean()
-            update = 0
+        keepitgoing = True
+        if tempangle.size != 0 and tempdistance is not None:
+            storeN = storeN + (superN / np.sum(superN) - pseudo_N / np.sum(pseudo_N)) / nrand1
 
-            tempang = N2[
-                
-                np.where((C[0] == 1) | (C[0] == tempdistance))[0][0] : np.where(C[0] == tempdistance)[0][0] + 1,
-                np.where((C[1] >= -tempangle - angle_bin) & (C[1] <= tempangle + angle_bin))[0][0] : 
-                np.where((C[1] >= -tempangle - angle_bin) & (C[1] <= tempangle + angle_bin))[0][-1] + 1
-            ]
+            while keepitgoing:
+                temp = N2[
+                    np.where(C[0] == 1)[0][0]: np.where(C[0] == tempdistance)[0][0] + 1,
+                    np.where(C[1] == -tempangle)[0][0]: np.where(C[1] == tempangle)[0][0] + 1
+                ]
 
-            tempdist = N2[
-                np.where((C[0] == 1))[0][0] : np.where((C[0] == tempdistance + distance_bin))[0][0] + 1,
-                np.where((C[1] == -tempangle))[0][0] : np.where((C[1] == tempangle))[0][0] + 1
-            ]
+                tempmean = temp.mean()
+                update = 0
 
-            tempangdist = N2[
-                np.where((C[0] == 1))[0][0] : np.where((C[0] == tempdistance + distance_bin))[0][0] + 1,
-                np.where((C[1] == -tempangle - angle_bin))[0][0] : np.where((C[1] == tempangle + angle_bin))[0][0] + 1
-            ]
+                tempang = N2[
+                    np.where((C[0] == 1) | (C[0] == tempdistance))[0][0]: np.where(C[0] == tempdistance)[0][0] + 1,
+                    np.where((C[1] >= -tempangle - angle_bin) & (C[1] <= tempangle + angle_bin))[0][0]:
+                    np.where((C[1] >= -tempangle - angle_bin) & (C[1] <= tempangle + angle_bin))[0][-1] + 1
+                ]
 
-            if np.mean(tempangdist) > np.mean(tempang) and np.mean(tempdist):
-                if np.prod(tempangdist.shape) * meanN2 > np.sum(tempang):
-                    update = 3
-                    tempangle += angle_bin
-                    tempdistance += distance_bin
+                tempdist = N2[
+                    np.where((C[0] == 1))[0][0]: np.where((C[0] == tempdistance + distance_bin))[0][0] + 1,
+                    np.where((C[1] == -tempangle))[0][0]: np.where((C[1] == tempangle))[0][0] + 1
+                ]
 
-            elif np.mean(tempang) > np.mean(tempdist):
-                if np.prod(tempang.shape) * meanN2 > np.sum(tempang) and np.mean(tempang) > tempmean:
-                    update = 1
-                    tempangle += angle_bin
+                tempangdist = N2[
+                    np.where((C[0] == 1))[0][0]: np.where((C[0] == tempdistance + distance_bin))[0][0] + 1,
+                    np.where((C[1] == -tempangle - angle_bin))[0][0]:np.where((C[1] == tempangle + angle_bin))[0][0] + 1
+                ]
 
-            else: 
-                if np.prod(tempang.shape) * meanN2 < np.sum(tempdist) and np.mean(tempdist) > tempmean:
-                    update = 2
-                    tempdistance += distance_bin
+                if np.mean(tempangdist) > np.mean(tempang) and np.mean(tempdist):
+                    if np.prod(tempangdist.shape) * meanN2 > np.sum(tempang):
+                        update = 3
+                        tempangle += angle_bin
+                        tempdistance += distance_bin
 
-            if update not in [1, 2, 3]: keepitgoing = False
+                elif np.mean(tempang) > np.mean(tempdist):
+                    if np.prod(tempang.shape) * meanN2 > np.sum(tempang) and np.mean(tempang) > tempmean:
+                        update = 1
+                        tempangle += angle_bin
 
-        angle[ni], distance[ni] = tempangle, tempdistance
-        
-        ## Time
-        pick_random_groups = {list(treatment.keys())[i]: list(treatment.values())[i] for i in temp_ind}
-        
-        args = [(list(pick_random_groups.values())[i], tempangle, tempdistance) for i in range(0, len(temp_ind))]
-        
-        with multiprocessing.Pool() as pool: tstrain = list(pool.map(SL.process_group, args))
+                else:
+                    if np.prod(tempang.shape) * meanN2 < np.sum(tempdist) and np.mean(tempdist) > tempmean:
+                        update = 2
+                        tempdistance += distance_bin
 
-        ptstrain = SL.boot_pseudo_times(treatment, nrand2, temp_ind, tempangle, tempdistance, start, exptime)
+                if update not in [1, 2, 3]:
+                    keepitgoing = False
 
-        M = np.arange(0, 30 * 60 + 0.05, 0.05)
-        N = np.zeros((len(ptstrain), len(M) - 1))
+            angle[ni], distance[ni] = tempangle, tempdistance
 
-        for i in range(len(ptstrain)):
-            temp = np.histogram(ptstrain[i][:-1], bins=M)[0]
-            temps = temp / np.sum(temp)
-            temps = np.cumsum(temps[::-1])[::-1]
-            N[i, :] = temps
+            # Time
+            pick_random_groups = {list(treatment.keys())[i]: list(treatment.values())[i] for i in temp_ind}
 
-        N[np.isnan(N)] = 0
-        PN, N = np.sum(N, axis=0), np.zeros((len(tstrain), len(M) - 1))
+            args = [(list(pick_random_groups.values())[i], tempangle, tempdistance) for i in range(0, len(temp_ind))]
 
-        for i in range(len(tstrain)):
-            temp = np.histogram(tstrain[i][:-1], bins=M)[0]
-            temps = temp / np.sum(temp)
-            temps = np.cumsum(temps[::-1])[::-1]
-            N[i, :] = temps
+            with multiprocessing.Pool() as pool:
+                tstrain = list(pool.map(SL.process_group, args))
 
-        N[np.isnan(N)] = 0
-        N = np.sum(N, axis=0)
-        temp = (N / n) - (PN / nrand2)
-        ftemp = np.where(temp == np.max(temp[: round(len(M) / 2)]))[0][0]
+            ptstrain = SL.boot_pseudo_times(treatment, nrand2, temp_ind, tempangle, tempdistance, start, exptime)
 
-        try:
-            keepgoing = True
-            while keepgoing:
-                if np.mean(temp[:ftemp]) < np.mean(temp[: ftemp + 1]): ftemp += 1
-                else: keepgoing = False
+            M = np.arange(0, 30 * 60 + 0.05, 0.05)
+            N = np.zeros((len(ptstrain), len(M) - 1))
 
-                if ftemp >= len(temp):
-                    ftemp -= 1
-                    keepgoing = False
+            for i in range(len(ptstrain)):
+                temp = np.histogram(ptstrain[i][:-1], bins=M)[0]
+                temps = temp / np.sum(temp)
+                temps = np.cumsum(temps[::-1])[::-1]
+                N[i, :] = temps
 
-            storeT[:, ni] = temp
-            ftemp = np.where(N * 0.5 < N[ftemp])[0]
+            N[np.isnan(N)] = 0
+            PN, N = np.sum(N, axis=0), np.zeros((len(tstrain), len(M) - 1))
 
-            if len(ftemp) > 0 and ftemp[0] != 0:
-                time_arr[ni] = M[ftemp[0]]
+            for i in range(len(tstrain)):
+                temp = np.histogram(tstrain[i][:-1], bins=M)[0]
+                temps = temp / np.sum(temp)
+                temps = np.cumsum(temps[::-1])[::-1]
+                N[i, :] = temps
 
-                # print(f"{ni} distance {distance[ni]} angle {angle[ni]} time {time_arr[ni]}")
-                d_df = pd.DataFrame([{"distance": distance[ni][0], "angle": angle[ni][0], "time": time_arr[ni][0]}])
-                criteria_df = pd.concat([criteria_df, d_df], ignore_index=True)
-                criteria_df.to_csv(CRITERIA_SAVE_PATH)
+            N[np.isnan(N)] = 0
+            N = np.sum(N, axis=0)
+            temp = (N / n) - (PN / nrand2)
+            ftemp = np.where(temp == np.max(temp[: round(len(M) / 2)]))[0][0]
 
-                times_path = f"/srv/milky/drosophila-datasets/{settings.TREATMENT}"
-                os.makedirs(times_path, exist_ok=True)
-                np.save(os.path.join(times_path, f"{ni}_real_array.npy"),  np.concatenate(tstrain))
-                np.save(os.path.join(times_path, f"{ni}_pseudo_array.npy"), np.concatenate(ptstrain))
-                
-                logger.info(f"{treatment_name} - {ni}: {time.time() - total_time}")
-                # sys.exit()
-                ni += 1
- 
-        except Exception as e:
-            print(e)
-            storeN = storeN - (superN / np.sum(superN) - pseudo_N / np.sum(pseudo_N)) / nrand1
-            distance[ni], angle[ni], time_arr[ni] = 0, 0, 0
+            try:
+                keepgoing = True
+                while keepgoing:
+                    if np.mean(temp[:ftemp]) < np.mean(temp[: ftemp + 1]):
+                        ftemp += 1
+                    else:
+                        keepgoing = False
+
+                    if ftemp >= len(temp):
+                        ftemp -= 1
+                        keepgoing = False
+
+                storeT[:, ni] = temp
+                ftemp = np.where(N * 0.5 < N[ftemp])[0]
+
+                if len(ftemp) > 0 and ftemp[0] != 0:
+                    time_arr[ni] = M[ftemp[0]]
+
+                    # print(f"{ni} distance {distance[ni]} angle {angle[ni]} time {time_arr[ni]}")
+                    d_df = pd.DataFrame([{"distance": distance[ni][0], "angle": angle[ni][0], "time": time_arr[ni][0]}])
+                    criteria_df = pd.concat([criteria_df, d_df], ignore_index=True)
+                    criteria_df.to_csv(CRITERIA_SAVE_PATH)
+
+                    times_path = f"/srv/milky/drosophila-datasets/{settings.TREATMENT}"
+                    os.makedirs(times_path, exist_ok=True)
+                    np.save(os.path.join(times_path, f"{ni}_real_array.npy"),  np.concatenate(tstrain))
+                    np.save(os.path.join(times_path, f"{ni}_pseudo_array.npy"), np.concatenate(ptstrain))
+
+                    logger.info(f"{treatment_name} - {ni}: {time.time() - total_time}")
+                    # sys.exit()
+                    ni += 1
+
+            except Exception:
+                # print(e)
+                storeN = storeN - (superN / np.sum(superN) - pseudo_N / np.sum(pseudo_N)) / nrand1
+                distance[ni], angle[ni], time_arr[ni] = 0, 0, 0
+
+    except Exception:
+        # print(e)
+        logger.info(f"{treatment_name} - {ni}: FAIL {time.time() - total_time}")
+        # storeN = storeN - (superN / np.sum(superN) - pseudo_N / np.sum(pseudo_N)) / nrand1
+        distance[ni], angle[ni], time_arr[ni] = 0, 0, 0
